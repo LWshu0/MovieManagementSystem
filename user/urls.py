27bec9bf82b7c8c9e6fd38969235ba92175de214
Liuwsh <eastwind10274@163.com>
2024-06-17 21:17:25 +0800

    merge: 合并其他分支中的文件

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("login", views.user_login, name="login"),
+    path("signup", views.user_signup, name="signup"),
+    path("home", views.user_home, name="home"),
+    path("myspace", views.user_myspace, name="myspace"),
 ]
